534a582561f311e4468d74557be98b6f71793688
Sandu Liviu Catalin <slc.universe@gmail.com>
2016-03-26 18:16:50 +0200

    Implement function to retrieve all active keybinds.

--- source/Entity/Keybind.cpp
+++ source/Entity/Keybind.cpp
@@ -192,6 +192,7 @@ static const Object & Keybind_FindByID(Int32 id)
     return NullObject();
 }
 
+// ------------------------------------------------------------------------------------------------
 static const Object & Keybind_FindByTag(CSStr tag)
 {
     // Perform a validity check on the specified tag
@@ -213,6 +214,35 @@ static const Object & Keybind_FindByTag(CSStr tag)
     }
     // Unable to locate a keybind matching the specified tag
     return NullObject();
+}
+
+// ------------------------------------------------------------------------------------------------
+static Array Keybind_FindActive()
+{
+    // Remember the initial stack size
+    StackGuard sg;
+    // Obtain the ends of the entity pool
+    Core::Keybinds::const_iterator itr = _Core->GetKeybinds().cbegin();
+    Core::Keybinds::const_iterator end = _Core->GetKeybinds().cend();
+    // Allocate an empty array on the stack
+    sq_newarray(DefaultVM::Get(), 0);
+    // Process each entity in the pool
+    for (; itr != end; ++itr)
+    {
+        // Is this entity instance active?
+        if (VALID_ENTITY(itr->mID))
+        {
+            // Push the script object on the stack
+            sq_pushobject(DefaultVM::Get(), (HSQOBJECT &)((*itr).mObj));
+            // Append the object at the back of the array
+            if (SQ_FAILED(sq_arrayappend(DefaultVM::Get(), -1)))
+            {
+                STHROWF("Unable to append entity instance to the list");
+            }
+        }
+    }
+    // Return the array at the top of the stack
+    return Var< Array >(DefaultVM::Get(), -1).value;
 }
 
 // ================================================================================================
@@ -245,6 +275,7 @@ void Register_CKeybind(HSQUIRRELVM vm)
         // Static Functions
         .StaticFunc(_SC("FindByID"), &Keybind_FindByID)
         .StaticFunc(_SC("FindByTag"), &Keybind_FindByTag)
+        .StaticFunc(_SC("FindActive"), &Keybind_FindActive)
         // Static Overloads
         .StaticOverload< Object & (*)(Int32, bool, Int32, Int32, Int32) >
             (_SC("CreateEx"), &Keybind_CreateEx)
