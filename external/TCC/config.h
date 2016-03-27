fdbd78cdd19f43c46419a3c2c7319357a9b4b6c9
Sandu Liviu Catalin <slc.universe@gmail.com>
2016-03-27 06:05:43 +0300

    Added the TCC compiler to the external folder and the associated projects.

---
+++ external/TCC/config.h
@@ -0,0 +1,7 @@
+#define CONFIG_TCCDIR "C:/Program Files (x86)/tcc"
+#define TCC_VERSION "0.9.26"
+
+/* #undef CONFIG_WIN32 */
+/* #undef CONFIG_WIN64 */
+/* #undef CONFIG_TCC_BCHECK */
+/* #undef CONFIG_TCC_ASSERT */
