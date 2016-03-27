5b52addbe9dd5e2c49f354a02d6850ead62ceff9
Sandu Liviu Catalin <slc.universe@gmail.com>
2016-03-27 21:24:42 +0300

    Remove extraneous character from printf inside the TCC module.

--- modules/tcc/Module.cpp
+++ modules/tcc/Module.cpp
@@ -387,7 +387,7 @@ void OutputMessageImpl(const char * msg, va_list args)
 
     SetConsoleTextAttribute(hstdout, csb_before.wAttributes);
 #else
-    printf("%c[0;32m[SQMOD]%c[0;37m", 27, 27, msg);
+    printf("%c[0;32m[SQMOD]%c[0;37m", 27, 27);
     vprintf(msg, args);
     puts("");
 #endif
@@ -410,7 +410,7 @@ void OutputErrorImpl(const char * msg, va_list args)
 
     SetConsoleTextAttribute(hstdout, csb_before.wAttributes);
 #else
-    printf("%c[0;32m[SQMOD]%c[0;37m", 27, 27, msg);
+    printf("%c[0;32m[SQMOD]%c[0;37m", 27, 27);
     vprintf(msg, args);
     puts("");
 #endif
