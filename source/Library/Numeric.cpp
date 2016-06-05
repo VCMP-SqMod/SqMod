17e78b83dd1d3bdba41591689b354825787b2d68
Sandu Liviu Catalin <slc.universe@gmail.com>
2016-06-05 03:54:15 +0300

    Initial untested implementation of the Decimal type.

--- source/Library/Numeric.cpp
+++ source/Library/Numeric.cpp
@@ -6,6 +6,7 @@ namespace SqMod {
 
 // ------------------------------------------------------------------------------------------------
 extern void Register_LongInt(HSQUIRRELVM vm);
+extern void Register_Decimal(HSQUIRRELVM vm);
 extern void Register_Math(HSQUIRRELVM vm);
 extern void Register_Random(HSQUIRRELVM vm);
 
@@ -13,6 +14,7 @@ extern void Register_Random(HSQUIRRELVM vm);
 void Register_Numeric(HSQUIRRELVM vm)
 {
     Register_LongInt(vm);
+    Register_Decimal(vm);
     Register_Math(vm);
     Register_Random(vm);
 }
