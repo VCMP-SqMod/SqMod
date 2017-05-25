b642e233a797a85e7f08f44eca5c3e2b5d68d9f1
Sandu Liviu Catalin <slc.universe@gmail.com>
2017-05-25 22:56:09 +0300

    Update the MaxmindDB library to the latest version.

--- config/mingw32/maxminddb_config.h
+++ config/mingw32/maxminddb_config.h
@@ -110,7 +110,7 @@
 #define PACKAGE_NAME "libmaxminddb"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libmaxminddb 1.2.0"
+#define PACKAGE_STRING "libmaxminddb 1.2.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libmaxminddb"
@@ -119,13 +119,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.2.0"
+#define PACKAGE_VERSION "1.2.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.2.0"
+#define VERSION "1.2.1"
 
 /* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
    <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
