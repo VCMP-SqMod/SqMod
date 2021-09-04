44ed4c849c6448f7816a5b2afc0a01e119b7739b
Sandu Liviu Catalin <slc.universe@gmail.com>
2021-09-05 00:30:00 +0300

    Update PocoMacros.cmake

--- vendor/POCO/cmake/PocoMacros.cmake
+++ vendor/POCO/cmake/PocoMacros.cmake
@@ -40,8 +40,15 @@ if(WIN32)
 			endforeach()
 		endif(X64)
 	endif()
-	find_program(CMAKE_MC_COMPILER mc.exe HINTS "${sdk_bindir}" "${kit_bindir}" "${kit81_bindir}" ${kit10_bindir}
-		DOC "path to message compiler")
+    find_program(CMAKE_MC_COMPILER mc.exe HINTS "${sdk_bindir}" "${kit_bindir}" "${kit81_bindir}" ${kit10_bindir}
+            DOC "path to message compiler")
+    elseif ("${CMAKE_GENERATOR}" MATCHES "MSYS" OR "${CMAKE_GENERATOR}" MATCHES "^(CodeBlocks)?.*(MinGW)?.*")
+    get_filename_component(MINGW_BIN_PATH ${CMAKE_C_COMPILER} DIRECTORY REALPATH)
+    if(NOT IS_DIRECTORY ${MINGW_BIN_PATH})
+        get_filename_component(MINGW_BIN_PATH ${CMAKE_CXX_COMPILER} DIRECTORY REALPATH)
+    endif()
+    find_program(CMAKE_MC_COMPILER windmc.exe HINTS "${MINGW_BIN_PATH}"
+            DOC "path to message compiler")
 	if(NOT CMAKE_MC_COMPILER)
 		message(FATAL_ERROR "message compiler not found: required to build")
 	endif(NOT CMAKE_MC_COMPILER)
